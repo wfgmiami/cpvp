970fb76ca6a801cc02db2019be87dc3983d4a211
Adash <wfgmiami@yahoo.com>
2016-01-31 17:45:17 +0000

    added login functionality

---
+++ classes/class.Authentication.php
@@ -0,0 +1,42 @@
+<?php
+	class TAuthentication {
+		function __construct() {
+			// Constructor
+
+			if ($_GET['logout'] = 1) {
+				$_SESSION['loggedin'] = 0;
+			}	
+			if ($_SESSION['loggedin'] == 1) {
+				
+				$this->isAuthorized = 1;
+			}
+			else {
+				$this->isAuthorized = 0;
+			}
+				
+		}
+		
+		function isAuthorized($username, $password) {
+			//echo "value is ".$this->isAuthorized."<br />";
+			return  $this->isAuthorized;
+		}
+		
+		function checkUserPass(){
+			if ($_POST['username'] == 'alex' && $_POST['password'] == '123'){ 		    		    return 1;
+                        } else {
+				return 0;
+			}	          	
+		}
+		function successfulLogin() {
+               		$_SESSION['loggedin'] = 1;
+                }
+
+		function failedLogin() {
+			$_SESSION['loggedin'] = 0;
+		}	
+
+
+				
+
+	}
+	
---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+// Controler
+	
+	require_once("../classes/class.Authentication.php");	
+	require_once("../classes/class.Session.php");
+
+	$Session 	= new TSession();
+	$Authentication = new TAuthentication();   	
+	
+	$ControllerVars['loggedin'] = 0;	
+	
+	if ($Authentication->isAuthorized()) {
+   	   	// logged in
+    		$ControllerVars['loggedin'] = 1;
+   	} else {
+		if ($_POST['submit'] == 'Submit') {
+			if ($Authentication->checkUserPass()) {
+				$ControllerVars['loggedin'] = 1;
+				$Authentication->successfulLogin();				
+			} else {
+				$Authentication->failedLogin();		
+			}
+		} 			
+	}
+	echo "Login status is: ".$ControllerVars['loggedin'];
+	if ($ControllerVars['loggedin'] == 0) {		
+		$content = file_get_contents("../templates/loginform.html");
+		echo $content;			 	
+	}		
+	die;
+	
+	if ($_SERVER['REQUEST_URI'] !== '/'){
+    	$content = file_get_contents("../templates/index.html");
+		preg_match('!name/([a-z]+)!imsx',$_SERVER['REQUEST_URI'], $pmatches);
+		$content = str_replace('{text}','Hello '. $pmatches[1], $content);	
+		echo $content;
+   	} else {
+      	       echo "you are not on the home page";
+   	}			
